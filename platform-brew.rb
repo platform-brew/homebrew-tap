20b7d26801a4d0c0e6bc820de9012f8d47840e6c
John Chilton <jmchilton@gmail.com>
2015-01-07 15:16:16 -0500

    Update platform-brew recipe to 0.2.0 (restores versions command).

--- platform-brew.rb
+++ platform-brew.rb
@@ -2,9 +2,12 @@ require 'formula'
 
 class PlatformBrew < Formula
   homepage 'https://github.com/jmchilton/platform-brew'
-  url 'https://github.com/jmchilton/platform-brew/archive/0.1.0.zip'
-  sha1 '59ac91db565759b63e208dbd1922074a0e895d63'
-  head 'https://github.com/jmchilton/platform-brew.git'
+  url 'https://github.com/jmchilton/platform-brew/archive/0.2.0.zip'
+  sha1 'fab422788244ecde86b0931da66241d0d1dd03e8'
+
+  head do
+    url 'https://github.com/jmchilton/platform-brew.git'
+  end
 
   def install
     bin.install Dir["./*"]
