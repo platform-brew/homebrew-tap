8576523b1901450de79cce312552b016731f7710
John Chilton <jmchilton@gmail.com>
2015-01-09 21:07:58 -0500

    Update recipe to target platform-brew organization on github.

--- platform-brew.rb
+++ platform-brew.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class PlatformBrew < Formula
-  homepage 'https://github.com/jmchilton/platform-brew'
-  url 'https://github.com/jmchilton/platform-brew/archive/0.2.0.zip'
+  homepage 'https://github.com/platform-brew/platform-brew'
+  url 'https://github.com/platform-brew/platform-brew/archive/0.2.0.zip'
   sha1 'fab422788244ecde86b0931da66241d0d1dd03e8'
 
   head do
-    url 'https://github.com/jmchilton/platform-brew.git'
+    url 'https://github.com/platform-brew/platform-brew.git'
   end
 
   def install
